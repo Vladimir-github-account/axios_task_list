6859b358e969cd7aca792ff0e5bf7568e9a4e63d
vladimir <s>
2020-03-02 22:57:39 +0200

    Very simple task list

--- src/api/index.js
+++ src/api/index.js
@@ -1,17 +1,18 @@
-import axios from 'axios';
+import axios        from 'axios';
+import { BASE_URL } from '../constants';
 
 axios.interceptors.request.use( function(config) {
   config.headers['authorization'] = '1';
+  return config;
 }, function(error) {
   return Promise.reject( error );
 } );
 
-export const createTask = async () => axios.post(
-    'http://localhost:5000/api/task',
-    {
-      value: 'Axios Task Value',
-      isDone: true,
-      deadline: '2020-03-30',
-      userId: 1
-    }
+export const createTask = async (task) => axios.post(
+    BASE_URL + 'task',
+    task
+);
+
+export const getUserTasks = async () => axios.get(
+    BASE_URL + 'tasks',
 );
--- src/components/TaskList/index.js
+++ src/components/TaskList/index.js
@@ -1,53 +1,102 @@
-import React, { Component } from 'react';
-import PropTypes            from 'prop-types';
-import { createTask }       from '../../api';
+import React, { Component }         from 'react';
+import { createTask, getUserTasks } from '../../api';
 
 class TaskList extends Component {
   constructor(props) {
     super( props );
     this.state = {
       isFetching: false,
-      task: null,
+      value: '',
+      deadline: '',
+      tasks: [],
       error: null,
     };
   }
 
-  componentDidUpdate(prevProps, prevState, snapshot) {
-    console.group( 'componentDidUpdate' );
-    console.log( prevState );
-    console.log( this.state );
-    console.groupEnd();
+  async componentDidMount() {
+    this.setState( {
+      isFetching: true,
+    } );
+    const { data } = await getUserTasks();
+    this.setState( {
+      tasks: data,
+    } );
   }
 
-  createTask = async () => {
+  createTask = async (e) => {
+    e.preventDefault();
     try {
       this.setState( {
-                       isFetching: true,
-                     } );
-      const { data } = await createTask();
+        isFetching: true,
+      } );
+      const { data } = await createTask( {
+        value: this.state.value,
+        isDone: this.state.isDone,
+        deadline: this.state.deadline,
+        userId: 1
+      } );
+      console.log( data );
+      console.log( this.state.tasks );
       this.setState( {
-                       task: data,
-                     } );
+        tasks: [ data, ...this.state.tasks ],
+      } );
     } catch (e) {
       this.setState( {
-                       error: e
-                     } );
+        error: `${e.response}`
+      } );
     } finally {
       this.setState( {
-                       isFetching: false
-                     } );
+        isFetching: false
+      } );
     }
   };
 
+  handleValueChange = (e) => {
+    const { name, type } = e.target;
+    this.setState( {
+      [name]: type === 'checkbox'
+          ? e.target.checked
+          : e.target.value
+    } );
+  };
+
   render() {
+    const { tasks, isFetching, error, value, deadline, isDone } = this.state;
     return (
-        <button onClick={this.createTask}>
-          CREATE TASK
-        </button>
+        <div>
+          <form onSubmit={this.createTask}>
+            <label>
+              <span>Task value</span>
+              <input type="text"
+                     name='value'
+                     onChange={this.handleValueChange}
+                     value={value}/>
+            </label>
+            <label>
+              <span>Deadline:</span>
+              <input type="date"
+                     name='deadline'
+                     onChange={this.handleValueChange}/>
+            </label>
+            <label>
+              <span>Is done?:</span>
+              <input onChange={this.handleValueChange}
+                     name='isDone'
+                     type="checkbox"/>
+            </label>
+            <input type='submit' value='CREATE TASK'/>
+          </form>
+          {isFetching && <h3>Loading...</h3>}
+          <ul>
+            {
+              tasks.map( item => (
+                  <li>{`${item.value} ${item.deadline} is done ${item.isDone}`}</li>
+              ) )
+            }
+          </ul>
+        </div>
     );
   }
 }
-
-TaskList.propTypes = {};
 
 export default TaskList;
